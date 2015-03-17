529f6457dd5bed1b64365adce7a6ee92d6295d0c
Donovan Preston <donovanpreston@gmail.com>
2015-03-17 18:11:17 -0400

    Better looking toolbar; login stubs; show the snippet in an img tag

--- src/views/frame.js
+++ src/views/frame.js
@@ -31,6 +31,7 @@ exports.Frame = React.createClass({
     }
 
     console.log(Object.getOwnPropertyNames(this.props.data));
+    console.log(Object.getOwnPropertyNames(this.props.meta));
     if (this.props.data.location) {
       let txt = this.props.data.location;
       if (txt.length > 50) {
@@ -46,12 +47,32 @@ exports.Frame = React.createClass({
       {this.props.snippet}
       {this.props.screenshot}
       <div id="toolbar">
-        <a href={this.props.data.location}>
+        <div class="logged-out" id="login-widget">
+          <div id="login-dropdown" class="login-collapsed">
+            <div id="login-compact">
+              <img src="https://pageshotpages.appspot.com/static/default-profile.svg" id="login-drop" class="user-avatar" style={{height: "20px", width: "20px"}} />
+            </div>
+            <div id="login-dropped">
+              <img src="https://pageshotpages.appspot.com/static/default-profile.svg" class="user-avatar" style={{height: "50px", width: "50px"}} />
+              <div class="if-logged-in">
+                <span class="user-email">-</span>
+                <br />
+                <span id="logout">logout</span>
+              </div>
+              <div class="if-logged-out">
+                Log in:
+                <input id="log-in-email" placeholder="email" type="text" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <a className="main-link" href={this.props.data.location}>
           {this.props.link_text_short}
+          <img src="/img/clipboard-8-xl.png" />
         </a>
       </div>
       <h1>{this.props.data.title || this.props.data.location}</h1>
-
+      <img src={this.props.meta.snippet} style={{width: "100%"}} />
       <iframe width="100%" id="frame" src={"/content/" + this.props.identifier} />
     </div>
   }
